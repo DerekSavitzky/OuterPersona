15d4bac57c2b83b50abd3330a4d6a5cb38084dcd
Derek Savitzky <dereksavitzky@gmail.com>
2013-11-21 10:44:28 -0600

    added associations and validations to catalogue for first attempt to test creating a catalogue

--- app/models/catalogue.rb
+++ app/models/catalogue.rb
@@ -1,3 +1,7 @@
 class Catalogue < ActiveRecord::Base
   attr_accessible :created_at, :description, :id, :name, :updated_at
+  has_many :products
+  
+  validates :id, :description, :name, :created_at, :updated_at, presence: true
+  
 end
