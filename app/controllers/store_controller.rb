ce9d4f525b645fb67e5b7db802a5eaf3cbeeaaed
Derek Savitzky <dereksavitzky@gmail.com>
2013-12-02 21:26:41 -0600

    Added a search that does not work great yet

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -6,4 +6,12 @@ class StoreController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+  
+  def search
+    #no code is required here
+  end #Automatically loads app/views/store/search.html.erb
+  
+  def search_results
+    @products = Product.where(" name LIKE  ?", "%#{params[:keyword]}%")
+  end
 end
